c7e6b1c0012afe830cd226df8c574f98a518b204
olivierdelcroix <odelcroi@gmail.com>
2026-08-31 15:13:26 +0200

    test

--- examples/api/src/views/Notifications.svelte
+++ examples/api/src/views/Notifications.svelte
@@ -1,12 +1,19 @@
 <script>
-  import { sendNotification } from '@tauri-apps/plugin-notification'
+  import {
+    sendNotification,
+    registerActionTypes,
+    setOnActionPerformed
+  } from '@tauri-apps/plugin-notification'
   export let onMessage
 
-  let sound = ''
+  Notification.requestPermission();
 
+  let sound = ''
+  let actionTypesRegistered = false
   // send the notification directly
   // the backend is responsible for checking the permission
   function _sendNotification() {
+    onMessage('send');
     sendNotification({
       title: 'Notification title',
       body: 'This is the notification body',
@@ -17,6 +24,7 @@
   // alternatively, check the permission ourselves
   function triggerNotification() {
     if (Notification.permission === 'default') {
+      onMessage('default');
       Notification.requestPermission()
         .then(function (response) {
           if (response === 'granted') {
@@ -27,10 +35,42 @@
         })
         .catch(onMessage)
     } else if (Notification.permission === 'granted') {
+      onMessage('granted');
       _sendNotification()
     } else {
       onMessage('Permission is denied')
     }
+  }
+
+  async function setupActionTypes() {
+    await registerActionTypes([
+      {
+        id: 'message',
+        actions: [{ id: 'reply', title: 'Reply' }]
+      }
+    ])
+    actionTypesRegistered = true
+    onMessage('Action types registered')
+  }
+
+  async function setupActionListener() {
+    await setOnActionPerformed((action) => {
+      onMessage(
+        'Action performed: ' +
+          action.actionId +
+          ' on notification ' +
+          action.notification.id
+      )
+    })
+    onMessage('Action listener registered')
+  }
+
+  function sendActionNotification() {
+    sendNotification({
+      title: 'Action Notification',
+      body: 'Click the Reply button or tap the body',
+      actionTypeId: 'message'
+    })
   }
 </script>
 
@@ -41,4 +81,14 @@
 />
 <button class="btn" id="notification" on:click={triggerNotification}>
   Send test notification
+</button>
+
+<button class="btn" on:click={setupActionTypes}>
+  Register action types
+</button>
+<button class="btn" on:click={setupActionListener}>
+  Set action listener
+</button>
+<button class="btn" on:click={sendActionNotification}>
+  Send action notification
 </button>
